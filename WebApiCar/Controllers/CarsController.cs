5addeb83c4e9544b283145de6b8767e9f85401d5
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-04-08 09:37:25 +0200

    tilføjet car controller og car model klasse

---
+++ WebApiCar/Controllers/CarsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCar.Model;
+
+namespace WebApiCar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        public static List<Car> carList = new List<Car>()
+        {
+            new Car(){Id = 1,Model="x3",Vendor="Tesla", Price=400000},
+            new Car(){Id = 2,Model="x2",Vendor="Tesla", Price=600000},
+            new Car(){Id = 3,Model="x1",Vendor="Tesla", Price=800000},
+            new Car(){Id = 4,Model="x0",Vendor="Tesla", Price=1400000},
+        };
+
+
+        // GET: api/Cars
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return carList;
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}", Name = "Get")]
+        public Car Get(int id)
+        {
+            return carList.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST: api/Cars
+        [HttpPost]
+        public void Post([FromBody] Car value)
+        {
+            Car newcar = new Car() { Id = GetId(), Model = value.Model, Vendor = value.Vendor, Price = value.Price };
+            carList.Add(newcar);
+        }
+
+        // PUT: api/Cars/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Car value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            carList.Remove(Get(id));
+        }
+
+       int GetId()
+        {
+            int max = carList.Max(x => x.Id);
+            return max+1;
+        }
+
+    }
+}
---
+++ WebApiCar/Model/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCar.Model
+{
+
+    /// <summary>
+    /// model klasse for car 
+    /// som bruges i web api'en
+    /// </summary>
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
