144785d941f902a21da8bdd485d8f3f6e620e227
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-05-15 10:25:21 +0200

    Added Swagger with SwashBuckle

--- WebApiCar/Startup.cs
+++ WebApiCar/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApiCar
 {
@@ -26,6 +27,26 @@ namespace WebApiCar
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Web Car API",
+                    Version = "v1",
+                    Description = "A simåle web API med basic CRUD operations",
+                    Contact = new Contact
+                    {
+                        Name = "Martin Kierkegaard",
+                        Email = string.Empty,
+                        Url =   string.Empty
+                    },
+                }
+                
+                );
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +60,16 @@ namespace WebApiCar
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API Car");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
